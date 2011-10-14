707c5b58722be418d3da012fff2183a92ca1c342
Krzysztof Menżyk <krzysztof.menzyk@netteam.pl>
2011-10-14 17:11:58 +0200

    DataTableBundle: Wyekstraktowanie metody w DataTableFactory

--- DataTableFactory.php
+++ DataTableFactory.php
@@ -18,11 +18,7 @@ class DataTableFactory
 
     public function get($name)
     {
-        if (!isset($this->datatables[$name])) {
-            throw new \InvalidArgumentException(sprintf('The DataTable "%s" is not defined.', $name));
-        }
-
-        $datatable = $this->container->get($this->datatables[$name]);
+        $datatable = $this->getDatatable($name);
         if ($datatable instanceof ContainerAwareInterface) {
             $datatable->setContainer($this->container);
         }
@@ -38,5 +34,23 @@ class DataTableFactory
     public function has($name)
     {
         return isset($this->datatables[$name]);
+    }
+
+    private function getDatatable($name)
+    {
+        if (!is_string($name)) {
+            throw new \InvalidArgumentException(sprintf('Expected argument of type "string", "%s" given', is_object($name) ? get_class($name) : gettype($name)));
+        }
+
+        if (!isset($this->datatables[$name])) {
+            throw new \InvalidArgumentException(sprintf('The DataTable "%s" is not defined.', $name));
+        }
+
+        $datatable = $this->container->get($this->datatables[$name]);
+        if (!$datatable instanceof DataTableInterface) {
+            throw new \InvalidArgumentException(sprintf('The service "%s" must implement DataTableInterface.', $name));
+        }
+
+        return $datatable;
     }
 }
