be3019f1325cf20f8a4ffd4c2fb08c4685b13daf
Krzysztof Menżyk <krzysztof.menzyk@netteam.pl>
2011-10-14 16:48:49 +0200

    DataTableBundle: zmian nazwy klasy DataTableContainerAware -> DataTable

--- DataTable.php
+++ DataTable.php
@@ -4,7 +4,7 @@ namespace NetTeam\System\DataTableBundle;
 
 use Symfony\Component\DependencyInjection\ContainerAware;
 
-abstract class DataTableContainerAware extends ContainerAware implements DataTableInterface
+abstract class DataTable extends ContainerAware implements DataTableInterface
 {
     protected function get($name)
     {
