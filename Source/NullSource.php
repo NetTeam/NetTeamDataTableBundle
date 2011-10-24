5b6951548506f33b3d27b99f531166194599c5bb
Krzysztof Menżyk <krzysztof.menzyk@netteam.pl>
2011-10-24 13:46:24 +0200

    DataTableBundle: Source dla pustych table -- NullSource

---
+++ Source/NullSource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace NetTeam\System\DataTableBundle\Source;
+
+/**
+ * Puste źródło danych
+ *
+ * @author Krzysztof Menżyk <krzysztof.menzyk@carrywater.pl>
+ */
+class NullSource implements SourceInterface
+{
+
+    public function getData($offset, $limit)
+    {
+        return array();
+    }
+
+    public function globalSearch(array $keys, $search)
+    {
+    }
+
+    public function addSorting($column, $order)
+    {
+    }
+
+    public function count()
+    {
+        return 0;
+    }
+}
