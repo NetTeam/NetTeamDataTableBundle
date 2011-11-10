056b62385b481049b1085a88ae48b0da86da1b0c
Wojciech Muła <wojciech.mula@netteam.pl>
2011-11-10 17:42:48 +0100

    + Kolumny: MoneyColumn (kwoty) i MoneyCurrencyColumn (kwoty i waluta) * Użycie MoneyCurrencyColumn w raportach windykacji
    Signed-off-by: Krzysztof Menżyk <krzysztof.menzyk@netteam.pl>

---
+++ Column/MoneyColumn.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace NetTeam\System\DataTableBundle\Column;
+
+/**
+ * MoneyColumn - kwota (liczba)
+ *
+ */
+class MoneyColumn extends Column
+{
+    protected $template = 'money_column';
+}
---
+++ Column/MoneyCurrencyColumn.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace NetTeam\System\DataTableBundle\Column;
+
+/**
+ * MoneyCurrencyColumn - kwota i waluta (liczba i napis)
+ *
+ */
+class MoneyCurrencyColumn extends Column
+{
+    protected $template = 'money_currency_column';
+}
