0017e19ec642f0845150d31ec0d141186fbb7ada
Krzysztof Menżyk <krzysztof.menzyk@netteam.pl>
2011-11-23 09:20:01 +0100

    DataTableBundle: Możliwość zdefiniowania separatora w ArrayColumn

--- Column/ArrayColumn.php
+++ Column/ArrayColumn.php
@@ -10,4 +10,31 @@ namespace NetTeam\System\DataTableBundle\Column;
 class ArrayColumn extends Column
 {
     protected $template = 'array_column';
+
+    protected $separator = ', ';
+
+    public function getValue($objectOrArray)
+    {
+        return array(
+            'items'     => parent::getValue($objectOrArray),
+            'separator' => $this->separator
+        );
+    }
+
+    public function setSeparator($separator)
+    {
+        $this->separator = $separator;
+
+        return $this;
+    }
+
+    public function separator($separator)
+    {
+        return $this->setSeparator($separator);
+    }
+
+    public function separatorNewLine()
+    {
+        return $this->setSeparator('<br />');
+    }
 }
