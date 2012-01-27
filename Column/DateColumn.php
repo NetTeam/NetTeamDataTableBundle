882bbc7bf8a1c21909eb281fe65696b3c5b68962
Wojciech Kulikowski <wojciech.kulikowski@carrywater.pl>
2012-01-27 13:27:16 +0100

    DataTableBundle: ustawianie formatu daty

--- Column/DateColumn.php
+++ Column/DateColumn.php
@@ -5,4 +5,35 @@ namespace NetTeam\System\DataTableBundle\Column;
 class DateColumn extends Column
 {
     protected $template = 'date_column';
+    protected $format = 'Y-m-d';
+
+    public static function create($caption, $getter, array $parameters)
+    {
+        $column = parent::create($caption, $getter, $parameters);
+
+        if (isset($parameters['format'])) {
+            $column->setFormat($parameters['format']);
+        }
+
+        return $column;
+    }
+    
+    public function getValue($objectOrArray)
+    {
+        $value = parent::getValue($objectOrArray);
+        $value->add('format', $this->format);
+
+        return $value;
+    }
+    
+    public function getFormat()
+    {
+        return $this->format;
+    }
+
+    public function setFormat($format)
+    {
+        $this->format = $format;
+    }
+
 }
