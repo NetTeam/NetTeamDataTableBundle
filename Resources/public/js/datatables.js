e542c83727d5390b9fc317c50857dda7ce764e83
Krzysztof Menżyk <krzysztof.menzyk@netteam.pl>
2012-07-05 16:04:35 +0200

    Poprawka w datatables.js

--- Resources/public/js/datatables.js
+++ Resources/public/js/datatables.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     $('.nt_datatable-reset-button').click(function(e) {
-        this.form.reset();
+        form = $(this).closest('form');
+        form[0].reset();
         e.preventDefault();
-        $('.nt_datatable-apply-button').click();
+        $('.nt_datatable-apply-button', form).click();
     });
 });
